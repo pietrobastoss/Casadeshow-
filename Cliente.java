f2acb899b9b515e658d52306f545ccdb8786c90c
Spock <lucasfernandezm09@gmail.com>
2025-11-26 21:21:30 -0300

    Verificada, Abstraidas e Corrigidas as Classes Relatorio e Cliente

--- Cliente.java
+++ Cliente.java
@@ -1,14 +1,55 @@
 public class Cliente {
     
     private String nome;
-    private int cpf; 
+    private int cpf;
+    private int idade;
+    private String email;
+    private int telefone;
+    
 
-    public Cliente(String n, int c) {
-        nome = nom; 
-        cpf = c;
+    public Cliente(String nome, int cpf, int idade, String email, int telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.email = email;
+        this.telefone = telefone;
     }
 
-    public String getNomee() { 
-        return nomeCompleto; 
+    public String getNome() {
+        return nome;
     }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [nome=" + nome + ", cpf=" + cpf + ", idade=" + idade + ", email=" + email + ", telefone="
+                + telefone + "]";
+    }
+
 }
